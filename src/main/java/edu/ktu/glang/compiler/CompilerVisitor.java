23ad1f654ba5f75a5873c4f687070e05e505e0ee
Evaldas Guogis <evaldasguogis@yahoo.com>
2023-04-06 23:00:53 +0300

    Print, assign, expression, operators implementation.

--- src/main/java/edu/ktu/glang/compiler/CompilerVisitor.java
+++ src/main/java/edu/ktu/glang/compiler/CompilerVisitor.java
@@ -25,17 +25,79 @@ public class CompilerVisitor extends GLangBaseVisitor<Object> implements Opcodes
         methodVisitor = classWriter.visitMethod(ACC_PUBLIC + ACC_STATIC, "main", "([Ljava/lang/String;)V", null, null);
         methodVisitor.visitCode();
 
-        // TODO this will print "Hello, world!"
-        methodVisitor.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
-        methodVisitor.visitLdcInsn("Hello, world!");
-        methodVisitor.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V", false);
-        // END
         super.visitProgram(ctx);
+
         methodVisitor.visitInsn(RETURN);
         methodVisitor.visitMaxs(0, 0); // Automatically computed by ASM
         methodVisitor.visitEnd();
 
         classWriter.visitEnd();
         return classWriter.toByteArray();
+    }
+
+    @Override
+    public Object visitPrintStatement(GLangParser.PrintStatementContext ctx) {
+        methodVisitor.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
+        visit(ctx.expression());
+        methodVisitor.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(I)V", false);
+        return null;
+    }
+
+    @Override
+    public Integer visitIntExpression(GLangParser.IntExpressionContext ctx) {
+        int value = Integer.parseInt(ctx.INT().getText());
+        methodVisitor.visitLdcInsn(value);
+        return value;
+    }
+
+    @Override
+    public Object visitAssignment(GLangParser.AssignmentContext ctx) {
+        String id = ctx.ID().getText();
+        int varIndex;
+        if (!symbolTable.contains(id)) {
+            varIndex = symbolTable.size();
+            symbolTable.put(id, varIndex);
+        } else {
+            varIndex = symbolTable.get(id);
+        }
+        visit(ctx.expression());
+        methodVisitor.visitVarInsn(ISTORE, varIndex);
+        return null;
+    }
+
+    @Override
+    public Integer visitIdExpression(GLangParser.IdExpressionContext ctx) {
+        String id = ctx.ID().getText();
+        int varIndex = symbolTable.get(id);
+        methodVisitor.visitVarInsn(ILOAD, varIndex);
+        return null;
+    }
+
+    @Override
+    public Object visitIntAddOpExpression(GLangParser.IntAddOpExpressionContext ctx) {
+        visit(ctx.expression(0));
+        visit(ctx.expression(1));
+        String op = ctx.intAddOp().getText();
+        if (op.equals("+")) {
+            methodVisitor.visitInsn(IADD);
+        } else if (op.equals("-")) {
+            methodVisitor.visitInsn(ISUB);
+        }
+        return null;
+    }
+
+    @Override
+    public Object visitIntMultiOpExpression(GLangParser.IntMultiOpExpressionContext ctx) {
+        visit(ctx.expression(0));
+        visit(ctx.expression(1));
+        String op = ctx.intMultiOp().getText();
+        if (op.equals("*")) {
+            methodVisitor.visitInsn(IMUL);
+        } else if (op.equals("/")) {
+            methodVisitor.visitInsn(IDIV);
+        } else if (op.equals("%")) {
+            methodVisitor.visitInsn(IREM);
+        }
+        return null;
     }
 }
--- src/main/java/edu/ktu/glang/compiler/SymbolTable.java
+++ src/main/java/edu/ktu/glang/compiler/SymbolTable.java
@@ -4,21 +4,25 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class SymbolTable {
-    private final Map<String, Object> table;
+    private final Map<String, Integer> table;
 
     public SymbolTable() {
         table = new HashMap<>();
     }
 
-    public void put(String name, Object value) {
+    public void put(String name, Integer value) {
         table.put(name, value);
     }
 
-    public Object get(String name) {
+    public Integer get(String name) {
         return table.get(name);
     }
 
     public boolean contains(String name) {
         return table.containsKey(name);
+    }
+
+    public int size() {
+        return table.size();
     }
 }
